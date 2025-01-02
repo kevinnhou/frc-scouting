f73a769d7ea6a3cbea4c6d9dba4c4b80496e0789
Kevin Hou <strawberrypi64@gmail.com>
2025-01-03 10:49:13 +1100

    fix: update links using button

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
 import { Text, View } from "react-native";
-import { Link } from "expo-router";
+
+import { Button } from "@/components/ui/Button";
 
 export default function Page() {
   return (
     <View>
       <Text>Welcome</Text>
-      <Link href="/match">Match Scouting</Link>
-      <Link href="/pit">Pit Scouting</Link>
+      <Button asChild href="/match">
+        <Text>Match Scouting</Text>
+      </Button>
+      <Button asChild href="/pit">
+        <Text>Pit Scouting</Text>
+      </Button>
     </View>
   );
 }
