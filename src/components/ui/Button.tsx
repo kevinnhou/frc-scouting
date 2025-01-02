3e57ee1981763e93f438b8abb0614274617e3172
Kevin Hou <strawberrypi64@gmail.com>
2025-01-03 10:48:52 +1100

    chore: add button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,68 @@
+import { TouchableOpacity, View, TouchableOpacityProps } from "react-native";
+import { Link } from "expo-router";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+export const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 duration-300 w-fit",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "h-9 w-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+interface ButtonProps
+  extends TouchableOpacityProps,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+  asChild?: boolean;
+  href?: string;
+}
+
+export function Button({
+  children,
+  variant,
+  size,
+  isLoading,
+  className,
+  asChild,
+  href,
+  ...props
+}: ButtonProps) {
+  const Comp = asChild && href ? Link : TouchableOpacity;
+
+  return (
+    <Comp
+      href={href}
+      disabled={isLoading}
+      className={cn(buttonVariants({ variant, size }), className)}
+      {...props}
+    >
+      <View>{children}</View>
+    </Comp>
+  );
+}
+
+Button.displayName = "Button";
