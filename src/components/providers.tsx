7d30a1bd65d89524bfba3e0f2344b5376b9ab2b9
Kevin Hou <strawberrypi64@gmail.com>
2025-01-25 09:56:12 +1100

    feat: add providers

---
+++ src/components/providers.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { type ReactNode } from "react";
+
+import { useTheme, ThemeProvider } from "next-themes";
+import { Toaster } from "sonner";
+
+function ToasterProvider() {
+  const { theme } = useTheme() as {
+    theme: "light" | "dark" | "system";
+  };
+  return <Toaster theme={theme} richColors position="bottom-center" />;
+}
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      enableSystem
+      disableTransitionOnChange
+      defaultTheme="system"
+    >
+      <ToasterProvider />
+      {children}
+    </ThemeProvider>
+  );
+}
