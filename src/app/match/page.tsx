65aab0af52ee1c2b96d37e87e1cca715cc8bc520
Kevin Hou <strawberrypi64@gmail.com>
2025-01-29 09:23:42 +1100

    feat: add match scouting form

---
+++ src/app/match/page.tsx
@@ -0,0 +1,9 @@
+import { MatchScoutingForm } from "@/components/form/match-scouting";
+
+export default function MatchScouting() {
+  return (
+    <div className="container mx-auto p-4">
+      <MatchScoutingForm />
+    </div>
+  );
+}
