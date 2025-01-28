dc47344dacecb0cf8e21ed3e11c89a9d6174e5f6
Kevin Hou <strawberrypi64@gmail.com>
2025-01-29 09:21:35 +1100

    feat: add input-field component

---
+++ src/components/form/input-field.tsx
@@ -0,0 +1,54 @@
+import { useFormContext } from "react-hook-form";
+
+import { Input } from "~/input";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/form";
+
+type TInputFieldProps = {
+  name: string;
+  label: string;
+  placeholder: string;
+  type?: string;
+};
+
+export function InputField({
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: TInputFieldProps) {
+  const { control } = useFormContext();
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              type={type}
+              {...field}
+              onChange={(e) => {
+                const value =
+                  type === "number"
+                    ? Number.parseFloat(e.target.value)
+                    : e.target.value;
+                field.onChange(value);
+              }}
+              autoComplete="off"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
