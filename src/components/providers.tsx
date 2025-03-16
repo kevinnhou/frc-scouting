f956a078c727117cdd35bc120e67510b640ceb4e
Kevin Hou <strawberrypi64@gmail.com>
2025-03-16 14:07:12 +1100

    fix: update sonner font

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,15 +1,30 @@
 "use client";
 
 import { type ReactNode } from "react";
+import { JetBrains_Mono } from "next/font/google";
 
 import { useTheme, ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
 function ToasterProvider() {
   const { theme } = useTheme() as {
     theme: "light" | "dark" | "system";
   };
-  return <Toaster theme={theme} richColors position="bottom-center" />;
+  return (
+    <Toaster
+      theme={theme}
+      richColors
+      position="bottom-center"
+      className={jetbrainsMono.className}
+    />
+  );
 }
 
 export default function Providers({ children }: { children: ReactNode }) {
