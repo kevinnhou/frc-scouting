17c059ab808ef4d1b9629c39adffc1f08323da55
Kevin Hou <strawberrypi64@gmail.com>
2025-03-18 18:54:23 +1100

    chore: add pwa icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -18,6 +18,17 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: "any",
         type: "image/x-icon",
       },
+      {
+        src: "/icons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/icons/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
     ],
   };
 }
