3df2118e6f14ed4002626de11eb92f109fcd94d2
Kevin Hou <strawberrypi64@gmail.com>
2025-03-18 21:21:35 +1100

    fix: force light mode

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -31,7 +31,7 @@ export default function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
-      enableSystem
+      // enableSystem
       disableTransitionOnChange
       defaultTheme="light"
     >
