4c239565af728656964dc250397a00e755268252
Kevin Hou <strawberrypi64@gmail.com>
2025-04-05 11:26:55 +1100

    feat: add wip banner

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,9 @@
+import { TextHover } from "~/text-hover";
+
 export default function Dashboard() {
-  return <></>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <TextHover text="WIP" />
+    </div>
+  );
 }
